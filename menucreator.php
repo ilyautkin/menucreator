b3bf4c3da9356d140038314855c43bc150d51298
Ilya Utkin <ilyautkin@mail.ru>
2015-06-16 13:26:35 +0300

    Последний подход

--- menucreator.php
+++ menucreator.php
@@ -1,73 +1,50 @@
 <?php
-ini_set('display_errors', -1);
-error_reporting(E_ALL);
+$tpl = $modx->getOption('tpl', $scriptProperties, 'row.menucreater');
+$outerTpl = $modx->getOption('outerTpl', $scriptProperties, 'outer.menucreater');
+$ulClass = $modx->getOption('ulClass', $scriptProperties, 'menu');
+$classes = $ulClass ? ' class="'.$ulClass.'"' : "";
+$showHideMenu = $modx->getOption('showHideMenu', $scriptProperties, 0);
+$showSubMenu = $modx->getOption('showSubMenu', $scriptProperties, 0);
+$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, PHP_EOL);
 
-//[[!devmenucreater? &tpl=`row.menucreater` &outerTpl=`outer.menucreator`]] Базовый запуск сниппета
-//$tpl - чанк для вывода меню. Если он пустой, меню не выводится. Переделать на вывод базового меню <ul> <li> <a> </li> </ul>
-//$outerTpl - чанк для обёртки вывода
-//$ulClass - класс для ul-контейнера меню
-//$showHideMenu - показ скрытых пунктов меню. Возможные значения только 1. Вывести ошибку в логи, если значение другое
-//$showSubMenu - показ пунктов подменю Возможные значения только 1. Вывести ошибку в логи, если значение другое
-
-$hidemenu = FALSE;
-$where = array('hidemenu'=>$hidemenu, 'published' => true, 'parent' => 0); //Стандартное условие для получения ресурсов
-
-if (isset($showHideMenu) && $showHideMenu == 1) //Показываем скрытые пункты меню
-{
-    unset($where['hidemenu']);
+$where = array('hidemenu' => 0, 'published' => 1, 'parent' => 0);
+if (isset($showHideMenu) && $showHideMenu == 1) {
+    $where['hidemenu'] = 1;
 }
-$output = "";
-$resources = $modx->getCollection('modResource',$where); //загружаем все ресурсы
-if (isset($showSubMenu) && $showSubMenu == 1) //Если показываем подменю
-{
-        $parentIDs = array(); // Сюда запишем все ID-шники первого уровня
-        foreach ($resources as $resource)
-        {
-            $parentIDs[] = $resource->get('id');
-        }
-        // Составляем запрос, чтобы выбрать ресурсы второго уровня
-        $subwhere = array('hidemenu'=>$hidemenu, 'published' => true, 'parent:IN'=>$parentIDs);
-        $subresources = $modx->getCollection('modResource',$subwhere);
-        $parents = array(); // Здесь будем хранить уже дерево ресурсов, разбитых на группы по parent
-        foreach ($subresources as $subresource)
-        {
-            $parentID = $subresource->get('parent');
-            $parents[$parentID][] = $subresource;
-        }
-}
-if (isset($tpl) && $tpl) //Если в вызове сниппета задан шаблон
-{
-        $template = $modx->getOption('tpl',$scriptProperties,$tpl); //загружаем наш чанк
-        foreach ($resources as $resource)
-        {
-            $resourceArray = $resource->toArray(); //забираем все параметры ресурса для передачи их в чанк
-            // Проверяем, были ли найдены дочерние ресурсы
-            if (isset($parents) && isset($parents[$resourceArray['id']]) && is_array($parents[$resourceArray['id']]))
-            {
-                $subOutput = "";
-                $subresources = $parents[$resourceArray['id']];
-                foreach ($subresources as $subresource)
-                {
-                    $subresourceArray = $subresource->toArray();
-                    $subOutput .= $modx->getChunk($template,$subresourceArray);
-                }
-            }
-            $resourceArray['output'] = $subOutput;
-            $output .= $modx->getChunk($template,$resourceArray);
-        }
+
+$resources = $modx->getCollection('modResource',$where);
+
+if ($showSubMenu == 1) {
+    $parentIDs = array();
+    foreach ($resources as $resource) {
+        $parentIDs[] = $resource->get('id');
+    }
+    
+    unset($where['parent']);
+    $where['parent:IN'] = $parentIDs;
+    $subresources = $modx->getCollection('modResource', $where);
+    
+    $parents = array();
+    foreach ($subresources as $subresource) {
+        $parentID = $subresource->get('parent');
+        $parents[$parentID][] = $subresource;
     }
 }
-else //Если в вызове сниппета не задан шаблон
-{
-    $output = 'empty tpl';
-}
 
-if (isset($ulClass) && $ulClass) //Если есть класс для ul обертки меню
-{
-    $classes = ' class="'.$ulClass.'"';
-}
-else //Если нет класса для ul обертки меню
-{
-    $classes = "";
+$output = array();
+foreach ($resources as $resource) {
+    $resArr = $resource->toArray();
+
+    if (isset($parents) && isset($parents[$resArr['id']]) && is_array($parents[$resArr['id']])) {
+        $subOutput = array();
+        $subresources = $parents[$resArr['id']];
+
+        foreach ($subresources as $subresource) {
+            $subresArr = $subresource->toArray();
+            $subOutput[] = $modx->getChunk($tpl,$subresArr);
+        }
+    }
+    $resArr['output'] = implode($outputSeparator, $subOutput);
+    $output[] = $modx->getChunk($tpl,$resArr);
 }
 return $modx->getChunk($outerTpl, array('output' => $output, 'classes' => $classes));
